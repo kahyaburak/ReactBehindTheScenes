d813826aa1e3993f1b493062d82831da9fcf5eb1
Burak <burakkahyabk@gmail.com>
2024-06-22 20:54:10 +0300

    Fixed counter not setting to the input value by using useEffect on initial count dependency

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import {useState, memo, useCallback, useMemo} from 'react';
+import {useState, memo, useCallback, useMemo, useEffect} from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -37,6 +37,10 @@ const Counter = memo(function Counter({initialCount}) {
   const [counterChanges, setCounterChanges] = useState([
     {value: initialCount, id: Math.random() * 1000},
   ]);
+
+  useEffect(() => {
+    setCounterChanges([{value: initialCount, id: Math.random() * 1000}]);
+  }, [initialCount]);
 
   const currentCounter = counterChanges.reduce(
     (prevCounter, counterChange) => prevCounter + counterChange.value,
